984d2aadc0741a56999c626956bc426a7d99a8d3
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-02 15:52:41 -0300

    chore: criando footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Header } from "./components/Header/Header";
 import { Banner } from "./components/Banner/Banner";
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Banner mensagem = "Encontre aqui o destino da sua próxima aventura!"/>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,43 +1,64 @@
 import React from 'react';
+// import ReactDOM from 'react-dom'
+import './footer.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { all } from '@awesome.me/kit-KIT_CODE/icons';
+import visaSvg from '../../assets/img/visa.svg';
+import mcSvg from '../../assets/img/mc.svg';
+import paypalSvg from '../../assets/img/paypal.svg';
+import dinersSvg from '../../assets/img/diners.svg';
+import amexSvg from '../../assets/img/amex.svg';
+import applepaySvg from '../../assets/img/applepay.svg';
+import paywithgoogleSvg from '../../assets/img/paywithgoogle.svg';
+import pixPng from '../../assets/img/pix.png';
 
+// library.add(...all);
 export class Footer extends React.Component {
     render () {
+        // const phone = <FontAwesomeIcon icon="fa-solid fa-phone" />
+        // ReactDOM.render(phone, document.body)
         return (
-            <footer>
-                <div class="secaoContato">
-                    <div class="contato" id="contato">
-                        <h3>Contato</h3>
-                        <p><i class="fa-solid fa-phone"></i>(98) 4002-8922</p>
-                        <p><i class="fa-brands fa-square-whatsapp"></i>(98) 98906-6255</p>
-                        <p><i class="fa-solid fa-envelope"></i>contato@exploramaranhao.com.br</p>
-                    </div>
-                    <div class="servicos">
-                        <h3>Serviços</h3>
-                        <p><a href="./index.html">Home</a></p>
-                        <p><a href="./front-end/paginas/sobre.html">Sobre</a></p>
-                        <p><a href="./front-end/paginas/destinos.html">Destinos</a></p>
-                    </div>
-                    <div class="formasDePagamento">
-                        <h3>Formas de Pagamento</h3>
-                        <div>
-                            <img src="./front-end/assets/img/visa.svg" alt="Visa"/>
-                            <img src="./front-end/assets/img/mc.svg" alt="MasterCard"/>
-                            <img src="./front-end/assets/img/paypal.svg" alt="PayPal"/>
-                            <img src="./front-end/assets/img/diners.svg" alt="Diners Club"/>
-                            <img src="./front-end/assets/img/amex.svg" alt="American Express"/>
-                            <img src="./front-end/assets/img/applepay.svg" alt="Apple Pay"/>
-                            <img src="./front-end/assets/img/paywithgoogle.svg" alt="Google Pay"/>
-                            <img src="./front-end/assets/img/pix.png" alt="Pix"/>
+            <body>
+                <footer>
+                    <div class="secaoContato">
+                        <div class="contato" id="contato">
+                            <h3>Contato</h3>
+                            {/* <p>{phone}</p> */}
+                            {/* // <p><FontAwesomeIcon icon={faPhone} />(98) 4002-8922</p> */}
+                            <p>(98) 4002-8922</p>
+                            <p><i class="fa-brands fa-square-whatsapp"></i>(98) 98906-6255</p>
+                            <p><i class="fa-solid fa-envelope"></i>contato@exploramaranhao.com.br</p>
+                        </div>
+                        <div class="servicos">
+                            <h3>Serviços</h3>
+                            <p><a href="../../../App.jsx">Home</a></p>
+                            <p><a href="#">Sobre</a></p>
+                            <p><a href="#">Destinos</a></p>
+                        </div>
+                        <div class="formasDePagamento">
+                            <h3>Formas de Pagamento</h3>
+                            <div>
+                                <img src={visaSvg} alt="Visa"/>
+                                <img src={mcSvg} alt="MasterCard"/>
+                                <img src={paypalSvg} alt="PayPal"/>
+                                <img src={dinersSvg} alt="Diners Club"/>
+                                <img src={amexSvg} alt="American Express"/>
+                                <img src={applepaySvg} alt="Apple Pay"/>
+                                <img src={paywithgoogleSvg} alt="Google Pay"/>
+                                <img src={pixPng} alt="Pix"/>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="rodape">
-                    <p>Desenvolvido por <a href="https://www.linkedin.com/in/kauan-guilherme-santos-a9a70a295/"
-                            class="linkLinkedIn">Kauan Guilherme Santos</a> e <a
-                            href="https://www.linkedin.com/in/ricardo-aguiar-537764200/" class="linkLinkedIn">Ricardo
-                            Aguiar</a>.</p>
-                </div>
-            </footer>
+                    <div class="rodape">
+                        <p>Desenvolvido por <a href="https://www.linkedin.com/in/kauan-guilherme-santos-a9a70a295/"
+                                class="linkLinkedIn">Kauan Guilherme Santos</a> e <a
+                                href="https://www.linkedin.com/in/ricardo-aguiar-537764200/" class="linkLinkedIn">Ricardo
+                                Aguiar</a>.</p>
+                    </div>
+                </footer>
+                <script src="https://kit.fontawesome.com/26cdbafd8c.js" crossorigin="anonymous"></script>
+            </body>
         )
     }
 }
