2e2a8856a15989996670156e791c7fe0e3d36a63
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-04 14:38:26 -0300

    feat: adicionando filtro de categoria na página destinos

--- frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
+++ frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
@@ -5,12 +5,55 @@ import { FaSearch } from "react-icons/fa";
 export class AreaPesquisa extends React.Component {
     render() {
         return (
-            <form className="caixaBuscaContainer" role="search">
-                <div className="caixaBusca">
-                    <input className="caixaBusca_input" type="search" placeholder="Pesquisar..." />
-                    <button className="caixaBusca_button" type="submit"><FaSearch /></button>
-                </div>
-            </form>
+            <body>
+                <form className="caixaBuscaContainer" role="search">
+                    <div className="caixaBusca">
+                        <input className="caixaBusca_input" type="search" placeholder="Pesquisar..." />
+                        <button className="caixaBusca_button" type="submit"><FaSearch /></button>
+                    </div>
+                </form>
+                <main>
+                    <aside>
+                        <h3>Categorias</h3>
+                        <ul className="list-filter">
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Praia 
+                            </label>
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Museu 
+                            </label>
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Eventos 
+                            </label>
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Parques 
+                            </label>
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Aventura 
+                            </label>
+                            <label className="filter">
+                                <input type="checkbox" />
+                                <span class="checkbox-custom"></span>
+                                Natureza 
+                            </label>
+                        </ul>
+                    </aside>
+                    <div>
+                        <h1>MAIN</h1>
+                        
+                    </div>
+                </main>
+            </body>
         )
     }
 }
