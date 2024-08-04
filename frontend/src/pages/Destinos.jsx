d9b66b193b757b98f7008748251e5417e05378fb
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-03 23:50:08 -0300

    feat: adicionando página destinos

--- frontend/src/pages/Destinos.jsx
+++ frontend/src/pages/Destinos.jsx
@@ -1,5 +1,13 @@
+import { Header } from "../components/Header/Header";
+import { Banner } from "../components/Banner/Banner";
+import { Footer } from "../components/Footer/Footer";
+
 export function Destinos() {
     return (
-        <h1>DESTINOS</h1>
+        <>
+            <Banner />
+            <Header />
+            <Footer />
+        </>
     )
 }
