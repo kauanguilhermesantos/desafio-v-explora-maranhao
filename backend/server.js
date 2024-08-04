18ca3cf96beab7cd4e38a88311a6c4ba3a46576f
ricardobr3no <kbreeno.18@gmail.com>
2024-08-04 19:58:28 -0300

    feat: add .env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`api escutando na porta http://localhost:${PORT} ...`);
