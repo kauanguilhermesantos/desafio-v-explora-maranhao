db7df7eb9601ba216427ba491e0a96929509e69b
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-04 10:36:11 -0300

    feat: adicionando banner da página destinos

--- frontend/src/pages/Destinos.jsx
+++ frontend/src/pages/Destinos.jsx
@@ -1,12 +1,14 @@
 import { Header } from "../components/Header/Header";
 import { Banner } from "../components/Banner/Banner";
 import { Footer } from "../components/Footer/Footer";
+import imgBannerDestinos from "../assets/img/banner3.jpg";
 
 export function Destinos() {
     return (
         <>
-            <Banner />
             <Header />
+            <Banner mensagem="Encontre seu próximo destino aqui!"
+            backgroundImage={imgBannerDestinos} />
             <Footer />
         </>
     )
