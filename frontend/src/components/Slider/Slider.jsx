2cd505c3c0aeb3fadfd9c7a8d68133d12cfdeea3
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-05 21:21:08 -0300

    feat: adicionando slider funcional

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,48 +1,50 @@
 import React from "react";
 import './slider.css';
 import Card from "../Card/Card";
-import { MdOutlineArrowBackIosNew } from "react-icons/md";
-import { MdOutlineArrowForwardIos } from "react-icons/md";
+// import { MdOutlineArrowBackIosNew } from "react-icons/md";
+// import { MdOutlineArrowForwardIos } from "react-icons/md";
 
+import { register } from "swiper/element/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/autoplay";
+import  Autoplay  from "swiper";
+
+register();
 
 export class Slider extends React.Component {
     render() {
+
+        const cards = Array.from({ length: 7 }, (_, index) => (
+            <Card key={index} />
+        ));
+    
         return (
-            <div class="slider-container">
-
-                <div class="slider-container-up">
-
-                    <div class="botoesDestinosMaisProcurado">
-                        <button id="anterior"><MdOutlineArrowBackIosNew /></button>
-                    </div>
-
-                    <div class="slider-wrapper">
-                        <div class="slider">
-                        
-                            <Card/>
-                            <Card/>
-                            <Card/>
-
-                        </div>
-                    </div>
-
-                    <div class="botoesDestinosMaisProcurado">
-                        <button id="proximo"><MdOutlineArrowForwardIos /></button>
-                    </div>
-
-                </div>
-
-
-            <ul class="pontinhosDestinosMaisProcurado">
-                <li class="ativo" data-index="0">•</li>
-                <li data-index="1">•</li>
-                <li data-index="2">•</li>
-                <li data-index="3">•</li>
-            </ul>
-            <script src="./slide.js"></script> 
-        </div>
-
-        )
+            <div className="slider">
+                <Swiper
+                    className="styleSlider"
+                    slidesPerView={3}
+                    pagination={{ clickable: true }}
+                    navigation
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={Autoplay}
+                >
+                        {cards.map((card, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="cardSlider">
+                                    {card}
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                </Swiper>
+            </div>
+        );
     }
     
 }
