564da400aab51b5b18d959faebcaf88bd4b7f09c
ricardobr3no <kbreeno.18@gmail.com>
2024-08-07 01:20:35 -0300

    fix : consertando filtro

--- frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
+++ frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
@@ -8,21 +8,28 @@ import Card from '../Card/Card';
 import api from "../../api";
 
 
-
-
-
 export default function AreaPesquisa() {
 
-    let tiposFiltros = [];
+    const tiposFiltros = ['Praia', 'Museu', 'Parque', 'Natureza', 'Aventura', 'Evento'];
+
 
     // api data
     const [atrativos, setAtrativo] = useState([]);
-    // checkbox-verification
-    const [isChecked, setIsChecked] = useState(false);
     // barra busca
     const [busca, setBusca] = useState('');
-    // click
-    const [isClicked, setIsClicked] = useState(false)
+    //
+    // const [selectedFilters, setSelectedFilters] = useState([]);
+    // const [atrativosComFiltro, setAtrativosComFiltro] = useState(atrativos);
+    //
+    // const handleFilterButtonClick = (selectedType) => {
+    //     if (selectedFilters.includes(selectedType)) {
+    //         let filters = selectedFilters.filter(el => el !== selectedType);
+    //         setSelectedFilters(filters);
+    //     } else {
+    //         setSelectedFilters([...selectedFilters, selectedType]);
+    //     }
+    //
+    // }
 
     async function getAtrativos() {
         const atrativosFromApi = await api.get("/atrativos");
@@ -32,24 +39,37 @@ export default function AreaPesquisa() {
 
     useEffect(() => {
         getAtrativos();
-    }, []);
+        // filterAtrativos();
+    }, [/* selectedFilters */]);
+
+
+    // const filterAtrativos = () => {
+    //     if (selectedFilters.length > 0) {
+    //         let tempAtrativos = selectedFilters.map(selectedCategory => {
+    //             let temp = atrativosFiltrados.filter(atrativo => atrativo.tipo === selectedCategory);
+    //             return temp;
+    //         });
+    //         setAtrativosComFiltro(tempAtrativos.flat());
+    //     } else {
+    //         setAtrativosComFiltro([...atrativosFiltrados])
+    //     }
+    // }
 
     // componente filtro
     const Filtro = ({ tipo }) => {
         return (
-            <label className="filter" id="filtro">
-                <input type="checkbox" 
-                // checked={isChecked} onChange={ev => setIsChecked(ev.currentTarget.value)} 
-                />
+            <label className="filter">
+                <input type="checkbox" /* onClick={() => handleFilterButtonClick(tipo)} */ />
                 <span class="checkbox-custom"></span>
                 {tipo}
             </label>
         )
     }
+
     // mexendo end
 
 
-    const atrativosFiltrados = atrativos.filter(atrativo => atrativo.nome.toLowerCase().startsWith(busca.toLowerCase()));
+    const atrativosFiltrados = atrativos.filter(atrativo => atrativo.nome.toLowerCase().includes(busca.toLowerCase()));
 
 
     return (
@@ -61,13 +81,13 @@ export default function AreaPesquisa() {
                         type="search"
                         placeholder="Pesquisar..."
                         value={busca}
-                        onChange={ev => setBusca(ev.target.busca)}
+                        onChange={ev => setBusca(ev.target.value)}
                     />
                     <button
                         className="caixaBusca_button"
                         type="button"
-                        // onClick={ev => setIsClicked(true)}
-                        >
+                        onClick={ev => setBusca(ev.target.value)}
+                    >
                         <FaSearch />
                     </button>
                 </div>
@@ -76,20 +96,19 @@ export default function AreaPesquisa() {
                 <aside>
                     <h3>Categorias</h3>
                     <ul className="list-filter">
-                        {/* parque museu parque aventura natureza Eventos */}
-                        <Filtro tipo={"Praia"} />
-                        <Filtro tipo={"Museu"} />
-                        <Filtro tipo={"Parque"} />
-                        <Filtro tipo={"Aventura"} />
-                        <Filtro tipo={"Natureza"} />
-                        <Filtro tipo={"Evento"} />
+
+                        { // filtros
+                            tiposFiltros.map((filtro, key) =>
+                                <Filtro tipo={filtro} key={key} />
+                            )
+                        }
+
                     </ul>
                 </aside>
                 <div>
-
                     { // cards dinamicos
                         atrativosFiltrados.map((props, key) =>
-                            <Card nome={props.nome} tipo={props.tipo} descricao={props.descricao} cidade={props.cidade} />
+                            <Card key={key} nome={props.nome} tipo={props.tipo} descricao={props.descricao} cidade={props.cidade} />
                         )
                     }
                 </div>
