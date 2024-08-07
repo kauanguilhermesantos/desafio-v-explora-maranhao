ebc1ce0a9a2776c19aaf941009d61ddaf9cf984d
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-07 10:57:04 -0300

    fix: adicionando url do back

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://api.render.com/deploy/srv-cqpnh9aj1k6c73dtn2k0?key=30PIcs1sD4c'
 });
 
 export default api;
