24ae9399de454aefdd6d220836e4235924fd61b5
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-08-08 00:24:06 -0300

    adicionando responsividade para a navbar

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,57 @@
 import React from 'react';
 import logoComTexto from '../../assets/logo/logo-com-texto-explora-maranhao.png';
+import logoSemTexto from '../../assets/logo/logo-sem-texto-explora-maranhao.png';
 import '../Reset/reset.css';
 import './header.css';
 import './hover.css';
+import { HiBars3 } from "react-icons/hi2";
+import { RiCloseLargeFill } from "react-icons/ri";
 
 import { Link } from "react-router-dom";
 
 export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMobile: window.innerWidth <= 834,
+            isTablet: window.innerWidth <= 480,
+            menuAtivo: false
+        };
+    }
+
+    atualizaTamanho = () => {
+        this.setState({
+            isMobile: window.innerWidth <= 834,
+            isTablet: window.innerWidth <= 480
+        });
+    };
+
+    ativaMenu = () => {
+        this.setState(estadoAntes => ({
+            menuAtivo: !estadoAntes.menuAtivo
+        }));
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.atualizaTamanho);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.atualizaTamanho);
+    }
     render () {
+        const { isMobile, menuAtivo, isTablet } = this.state;
         return (
             <header>
                 <nav className="barraDeNavegacao">
-                        <Link to="/"><img src={logoComTexto}
-                                alt="Logo do Explora Maranhão" className="logoComTexto"/></Link>
+                        <Link to="/"><img src={isMobile ? logoSemTexto : logoComTexto} alt="Logo do Explora Maranhão" className="logoComTexto"/></Link>
                         {/* <a href="index.html"></a> */}
-                        <ul className="navMenu">
+                        {isTablet && (
+                            <button className={`menuToggle ${menuAtivo ? 'roda' : ''}`} onClick={this.ativaMenu}>
+                                {menuAtivo ? <RiCloseLargeFill /> : <HiBars3 />}
+                            </button>
+                        )}
+                        <ul className={`navMenu ${menuAtivo ? 'ativo' : ''}`}>
                             <li className="listaMenu"><Link className="botoesDoMenu" to="/">Home</Link></li>
                             {/* <li ><a href="index.html" >Home</a></li> */}
                             <li className="listaMenu"><Link className="botoesDoMenu" to="/sobre">Sobre</Link></li>
