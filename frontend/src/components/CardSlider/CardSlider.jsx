36fd9b723abbe07b2dfa9471d3d5a113a2c4d288
ricardobr3no <130100830+ricardobr3no@users.noreply.github.com>
2024-08-08 22:52:08 -0300

    feat: alterei Slider e conectei com api

---
+++ frontend/src/components/CardSlider/CardSlider.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./card.css";
+// import imagemTeste from "../../assets/img/1.png";
+import Modal from "react-modal";
+// import parse from "html-react-parser"
+import { useState, useEffect, useRef } from "react";
+//
+
+
+Modal.setAppElement("#root");
+
+export default function CardSlider(props) {
+
+
+  // const [modalIsOpen, setIsOpen] = useState(false);
+  // const modalRef = useRef();
+
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
+
+  // function closeModalButton(event) {
+  //   event.stopPropagation();
+  //   closeModal();
+  // }
+
+
+  // function closeModal() {
+  //   setIsOpen(false);
+  // }
+
+  // function handleClickOutside(event) {
+  //   if (modalRef.current && !modalRef.current.contains(event.target)) {
+  //     closeModal();
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   if (modalIsOpen) {
+  //     document.addEventListener("mousedown", handleClickOutside);
+  //   } else {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   }
+  //
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [modalIsOpen]);
+  //
+
+
+
+  return (
+    <div className="cardDestinosMaisProcurados" /* onClick={openModal} */>
+      <img src={props.imagemSource} alt="Imagem teste" className="imagemCardDestinoMaisProcurado" />
+      <div className="infoCardDestinosMaisProcurado">
+        <p className="NomeCardDestinoMaisProcurado">{props.nome}</p>
+        <div>
+          <p className="cidadeCardDestinoMaisProcurado">{props.cidade}</p>
+          <span className="tipoAtrativoCardDestinoMaisProcurado">{props.tipo}</span>
+        </div>
+      </div>
+      {/* <Modal */}
+      {/*   isOpen={modalIsOpen} */}
+      {/*   onRequestClose={closeModal} */}
+      {/*   contentLabel="Modal" */}
+      {/*   overlayClassName="modal-overlay" */}
+      {/*   className="modal-content" */}
+      {/* // shouldCloseOnOverlayClick={true} */}
+      {/* > */}
+      {/*   <div ref={modalRef}> */}
+      {/*     <img src={props.imagemSource} alt="imagem atrativo" className="imagemModal" /> */}
+      {/*     {console.log(props.imagemSource)} */}
+      {/*     <div className="infoModal"> */}
+      {/*       <button onClick={closeModalButton} className="botaoModal">X</button> */}
+      {/*       <h4>{props.nome}</h4> */}
+      {/*       <p className="cidadeModal">{props.cidade}</p> */}
+      {/*       <p className="descricaoModal">{props.descricao}</p> */}
+      {/*     </div> */}
+      {/*   </div> */}
+      {/**/}
+      {/**/}
+      {/*   <div className="mapaModal" id="mapaIframe"> */}
+      {/*     {parse(props.mapa)} */}
+      {/*   </div> */}
+      {/* </Modal> */}
+    </div>
+  )
+} 
--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './slider.css';
-import Card from "../Card/Card";
+import CardSlider from "../CardSlider/CardSlider.jsx";
 // import { MdOutlineArrowBackIosNew } from "react-icons/md";
 // import { MdOutlineArrowForwardIos } from "react-icons/md";
 
@@ -11,41 +11,75 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/autoplay";
-import  Autoplay  from "swiper";
+import Autoplay from "swiper";
+import { useState, useEffect } from "react";
+
+
+import api from "../../api.js";
+
 
 register();
 
-export class Slider extends React.Component {
-    render() {
+export function Slider() {
+    // export class Slider extends React.Component {
+    // render() {
 
-        const cards = Array.from({ length: 7 }, (_, index) => (
-            <Card key={index} />
-        ));
-    
-        return (
-            <div className="slider">
-                <Swiper
-                    className="styleSlider"
-                    slidesPerView={3}
-                    pagination={{ clickable: true }}
-                    navigation
-                    autoplay={{
-                        delay: 3000,
-                        disableOnInteraction: false,
-                    }}
-                    modules={Autoplay}
-                >
-                        {cards.map((card, index) => (
-                            <SwiperSlide key={index}>
-                                <div className="cardSlider">
-                                    {card}
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                </Swiper>
-            </div>
-        );
+    const [atrativos, setAtrativos] = useState([]);
+
+    async function getAtrativos() {
+        const atrativosFromApi = await api.get("/atrativos");
+        // console.clear();
+        // console.log(atrativosFromApi.data);
+        setAtrativos(atrativosFromApi.data);
     }
-    
+
+    useEffect(() => {
+        getAtrativos();
+    }, [])
+
+
+
+    const cards = Array.from({ length: 7 }, (_, index) => (
+        <CardSlider
+            key={index}
+            nome={atrativos[index].nome}
+            tipo={atrativos[index].tipo}
+            imagemSource={atrativos[index].imagemSource}
+        />
+    ));
+
+
+    console.clear();
+
+    // const cards = atrativos.slice(0, 4).forEach((atrativo) => {
+    //     <CardSlider />
+    // });
+    //
+
+
+    return (
+        <div className="slider">
+            <Swiper
+                className="styleSlider"
+                slidesPerView={3}
+                pagination={{ clickable: true }}
+                navigation
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                modules={Autoplay}
+            >
+                {cards.map((card, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="cardSlider">
+                            {card}
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+    // }
 }
 
