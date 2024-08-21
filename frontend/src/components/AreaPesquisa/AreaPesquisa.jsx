a69d3e2a47b3a550380bcf85ccc57b40fa098284
ricardobr3no <kbreeno.18@gmail.com>
2024-08-21 17:46:10 -0300

    fix: consertei filtros

--- frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
+++ frontend/src/components/AreaPesquisa/AreaPesquisa.jsx
@@ -7,131 +7,131 @@ import Card from "../Card/Card";
 import api from "../../api";
 
 export default function AreaPesquisa() {
-    const tiposFiltros = [
-        "Praia",
-        "Museu",
-        "Parque",
-        "Natureza",
-        "Aventura",
-        "Evento",
-    ];
+  const tiposFiltros = [
+    "Praia",
+    "Museu",
+    "Parque",
+    "Natureza",
+    "Aventura",
+    "Evento",
+  ];
 
-    // api data
-    const [atrativos, setAtrativo] = useState([]);
-    // barra busca
-    const [busca, setBusca] = useState("");
-    // filters
-    const [selectedFilters, setSelectedFilters] = useState([]);
-    // const [atrativosComFiltro, setAtrativosComFiltro] = useState(atrativos);
+  // api data
+  const [atrativos, setAtrativo] = useState([]);
+  // barra busca
+  const [busca, setBusca] = useState("");
+  // filters
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  // const [atrativosComFiltro, setAtrativosComFiltro] = useState(atrativos);
 
-    // function para pegar resultado da requisição da api
-    async function getAtrativos() {
-        const atrativosFromApi = await api.get("/atrativos");
-        console.log(atrativosFromApi.data);
-        setAtrativo(atrativosFromApi.data);
+  // function para pegar resultado da requisição da api
+  async function getAtrativos() {
+    const atrativosFromApi = await api.get("/atrativos");
+    console.log(atrativosFromApi.data);
+    setAtrativo(atrativosFromApi.data);
+  }
+
+  useEffect(() => {
+    getAtrativos();
+  }, [selectedFilters]);
+
+  const handleCheckboxChange = (category) => {
+    setSelectedFilters((prevSelectedFilters) =>
+      prevSelectedFilters.includes(category)
+        ? prevSelectedFilters.filter((c) => c !== category)
+        : [...prevSelectedFilters, category],
+    );
+  };
+
+  const atrativosFiltrados = atrativos.filter(
+    (atrativo) =>
+      atrativo.nome.toLowerCase().includes(busca.toLowerCase()) ||
+      atrativo.cidade.toLowerCase().includes(busca.toLowerCase()),
+  );
+
+  document.addEventListener("keypress", (tecla) => {
+    if (tecla.key === "Enter") {
+      const searchContent = document.getElementById("conteudo-busca").value;
+      tecla.preventDefault();
+      // console.log(searchContent);
+      setBusca(searchContent);
     }
+  });
 
-    useEffect(() => {
-        getAtrativos();
-    }, [selectedFilters]);
+  const filteredItems = atrativosFiltrados.filter(
+    (item) =>
+      selectedFilters.length === 0 || selectedFilters.includes(item.tipo),
+  );
 
-    const handleCheckboxChange = (category) => {
-        setSelectedFilters((prevSelectedFilters) =>
-            prevSelectedFilters.includes(category)
-                ? prevSelectedFilters.filter((c) => c !== category)
-                : [...prevSelectedFilters, category],
-        );
-    };
-
-    const atrativosFiltrados = atrativos.filter(
-        (atrativo) =>
-            atrativo.nome.toLowerCase().includes(busca.toLowerCase()) ||
-            atrativo.cidade.toLowerCase().includes(busca.toLowerCase()),
-    );
-
-    document.addEventListener("keypress", (tecla) => {
-        if (tecla.key == "Enter") {
-            const searchContent = document.getElementById("conteudo-busca").value;
-            tecla.preventDefault();
-            // console.log(searchContent);
-            setBusca(searchContent);
-        }
-    });
-
-    const filteredItems = atrativosFiltrados.filter(
-        (item) =>
-            selectedFilters.length === 0 || selectedFilters.includes(item.tipo),
-    );
-
-    // componente filtro
-    const Filtro = ({ tipo }) => {
-        return (
-            <label className="filter">
-                <input
-                    type="checkbox"
-                    value={tipo}
-                    onChange={() => handleCheckboxChange(tipo)}
-                    checked={selectedFilters.includes(tipo)}
-                />
-                <span className="checkbox-custom"></span>
-                {tipo}
-            </label>
-        );
-    };
-
+  // componente filtro
+  const Filtro = ({ tipo }) => {
     return (
-        <body>
-            <form className="caixaBuscaContainer" role="search">
-                <div className="caixaBusca">
-                    <input
-                        id="conteudo-busca"
-                        className="caixaBusca_input"
-                        type="search"
-                        placeholder="Pesquisar..."
-                    />
-                    <button
-                        className="caixaBusca_button"
-                        type="button"
-                        onClick={() => {
-                            const searchContent =
-                                document.getElementById("conteudo-busca").value;
-                            // console.log(searchContent);
-                            setBusca(searchContent);
-                        }}
-                    >
-                        <FaSearch />
-                    </button>
-                </div>
-            </form>
-            <main>
-                <aside>
-                    <h3>Categorias</h3>
-                    <ul className="list-filter">
-                        {
-                            // filtros
-                            tiposFiltros.map((tipoFiltro, key) => (
-                                <Filtro tipo={tipoFiltro} key={key} />
-                            ))
-                        }
-                    </ul>
-                </aside>
-                <div>
-                    {
-                        // cards dinamicos
-                        atrativosFiltrados.map((props, key) => (
-                            <Card
-                                key={key}
-                                nome={props.nome}
-                                tipo={props.tipo}
-                                mapa={props.mapa}
-                                descricao={props.descricao}
-                                cidade={props.cidade}
-                                imagemSource={props.imagemSource}
-                            />
-                        ))
-                    }
-                </div>
-            </main>
-        </body>
+      <label className="filter">
+        <input
+          type="checkbox"
+          value={tipo}
+          onChange={() => handleCheckboxChange(tipo)}
+          checked={selectedFilters.includes(tipo)}
+        />
+        <span className="checkbox-custom"></span>
+        {tipo}
+      </label>
     );
+  };
+
+  return (
+    <body>
+      <form className="caixaBuscaContainer" role="search">
+        <div className="caixaBusca">
+          <input
+            id="conteudo-busca"
+            className="caixaBusca_input"
+            type="search"
+            placeholder="Pesquisar..."
+          />
+          <button
+            className="caixaBusca_button"
+            type="button"
+            onClick={() => {
+              const searchContent =
+                document.getElementById("conteudo-busca").value;
+              // console.log(searchContent);
+              setBusca(searchContent);
+            }}
+          >
+            <FaSearch />
+          </button>
+        </div>
+      </form>
+      <main>
+        <aside>
+          <h3>Categorias</h3>
+          <ul className="list-filter">
+            {
+              // filtros
+              tiposFiltros.map((tipoFiltro, key) => (
+                <Filtro tipo={tipoFiltro} key={key} />
+              ))
+            }
+          </ul>
+        </aside>
+        <div>
+          {
+            // cards dinamicos
+            filteredItems.map((props, key) => (
+              <Card
+                key={key}
+                nome={props.nome}
+                tipo={props.tipo}
+                mapa={props.mapa}
+                descricao={props.descricao}
+                cidade={props.cidade}
+                imagemSource={props.imagemSource}
+              />
+            ))
+          }
+        </div>
+      </main>
+    </body>
+  );
 }
